07ced4b2ec3137861b9c2d45aa9f15916a3ebbf1
Carlos Garcia Galarce <cbgarcia1@uc.cl>
2017-03-14 17:03:02 -0300

    Scaffold Articles

---
+++ db/migrate/20170314195717_create_articles.rb
@@ -0,0 +1,11 @@
+class CreateArticles < ActiveRecord::Migration[5.0]
+  def change
+    create_table :articles do |t|
+      t.string :headline
+      t.string :lead
+      t.string :body
+
+      t.timestamps
+    end
+  end
+end
